bfa50675d3f2870f7efd1eef859ec6d29701e478
ErrorCodez Bots <147271056+ErrorCodez-Bots@users.noreply.github.com>
2025-10-04 20:12:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 from typing import List
 
-API_ID = os.environ.get("API_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = os.environ.get("API_ID", "20293219")
+API_HASH = os.environ.get("API_HASH", "4aef7d9e065d92f4a95736eaeb93d3ac")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8383692292:AAGEO7N8LliIeKB6fciWRd_MzBIBKSia4lc")
-ADMIN = int(os.environ.get("ADMIN", ""))
-PICS = (os.environ.get("PICS", "")).split()
+ADMIN = int(os.environ.get("ADMIN", "6091537598"))
+PICS = (os.environ.get("PICS", "https://telegra.ph/file/7a408e30f2beeb78b5020-af7cbf2907f8d842ea.jpg")).split()
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "False").lower() == "true"  # Set "True" For accept new requests
